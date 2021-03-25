f875b2d15bc363c87b63a80ac9de8774f62fe983
itdar <thesockerrr@gmail.com>
2021-03-26 01:19:35 +0900

    Solved coin zero (11047) problem in acmicpc

---
+++ acmicpc/greedy_11047_coin_zero.java
@@ -0,0 +1,28 @@
+package acmicpc;
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int coinNumber = sc.nextInt();
+        int goalValue = sc.nextInt();
+        int[] coins = new int[coinNumber];
+        for(int i = 0; i < coinNumber; ++i)
+            coins[i] = sc.nextInt();
+
+        int count = 0;
+
+        int validCount;
+        for (int i = coins.length-1; i >= 0; --i) {
+            validCount = goalValue / coins[i];
+            count += validCount;
+            goalValue = goalValue - (coins[i] * validCount);
+        }
+
+        System.out.println(count);
+    }
+
+}
