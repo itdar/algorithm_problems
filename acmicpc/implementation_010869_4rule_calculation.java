3a9bd31f824ab80b8abbba62261cb4a7b186303a
itdar <thesockerrr@gmail.com>
2021-03-29 23:20:25 +0900

    Solved impletation prob in baekjoon acmicpc

---
+++ acmicpc/implementation_010869_4rule_calculation.java
@@ -0,0 +1,30 @@
+package acmicpc;
+
+import java.io.*;
+import java.util.*;
+
+class Calculation {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        // bw.write(a+b);
+        // bw.write(a-b);
+        // bw.write(a*b);
+        // bw.write(a/b);
+        // bw.write(a%b);
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        // bw.flush();
+        // bw.close();
+    }
+}
