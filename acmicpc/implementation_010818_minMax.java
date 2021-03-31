d678000afe6926ee77f407b936a9db4e8e230218
itdar <thesockerrr@gmail.com>
2021-04-01 00:02:29 +0900

    Solved 010818 in acmicpc baekjoon

---
+++ acmicpc/implementation_010818_minMax.java
@@ -0,0 +1,25 @@
+package acmicpc;
+
+import java.io.*;
+import java.util.*;
+
+class MinMax {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numCount = Integer.parseInt(br.readLine());
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
+        long min = +1000000;
+        long max = -1000000;
+        long temp;
+        for (int i = 0; i < numCount; ++i) {
+            temp = Long.parseLong(tokenizer.nextToken());
+            if (min > temp) {
+                min = temp;
+            }
+            if (max < temp) {
+                max = temp;
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
