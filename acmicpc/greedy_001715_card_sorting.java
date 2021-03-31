0223ecc3faf73c00e5cb4d26f4663132dc4845a8
itdar <thesockerrr@gmail.com>
2021-03-31 23:18:35 +0900

    Solved 001715 in acmicpc baekjoon

---
+++ acmicpc/greedy_001715_card_sorting.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class CardSorting {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cards = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> cardPacks = new PriorityQueue<>();
+        for (int i = 0; i < cards; ++i)
+            cardPacks.add(Integer.parseInt(br.readLine()));
+
+        int comparisonSum = 0;
+        int first;
+        int second;
+        while (cardPacks.size() > 1) {
+            first = cardPacks.poll();
+            second = cardPacks.poll();
+            comparisonSum += (first + second);
+            cardPacks.add(first+second);
+        }
+
+        System.out.println(comparisonSum);
+    }
+}
