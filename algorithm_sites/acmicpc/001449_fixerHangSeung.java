0ff38747937aa70bfabc336e533661badc952708
itdar <thesockerrr@gmail.com>
2021-04-13 00:47:16 +0900

    Process of 1449 acmicpc

--- algorithm_sites/acmicpc/001449_fixerHangSeung.java
+++ algorithm_sites/acmicpc/001449_fixerHangSeung.java
@@ -2,6 +2,13 @@ package algorithm_sites.acmicpc;
 
 import java.util.*;
 
+// Process
+// 1. Input 새는곳개수N, 테이프길이L, 새는 위치들(좌측으로부터의)
+// 2. 새는 위치들 정렬한다.
+// 3. 새는 곳들 전체 반복한다.
+//  3.1. 테이프 길이로 최대 덮을 수 있는 범위에 포함되지 않으면, 테이프를 추가하며 센다.
+// 4. 센 개수 반환한다.
+
 class FixerHangSeung {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
