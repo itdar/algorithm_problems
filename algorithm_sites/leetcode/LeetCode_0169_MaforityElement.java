a45e53691ccc422a8bc7becb778561b0cbca7b00
itdar <thesockerrr@gmail.com>
2021-09-16 19:58:42 +0900

    chore: Solved LeetCode_0169_MaforityElement, improved little

--- algorithm_sites/leetcode/LeetCode_0169_MaforityElement.java
+++ algorithm_sites/leetcode/LeetCode_0169_MaforityElement.java
@@ -2,12 +2,14 @@ package algorithm_sites.leetcode;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class LeetCode_0169_MaforityElement {
 
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
+
+        int max = Integer.MIN_VALUE;
+        int result = 0;
 
         for (int num : nums) {
             if (map.containsKey(num)) {
@@ -15,20 +17,22 @@ public class LeetCode_0169_MaforityElement {
             } else {
                 map.put(num, 1);
             }
-        }
 
-        int max = Integer.MIN_VALUE;
-        int result = 0;
-
-        Set<Integer> keys = map.keySet();
-        for (Integer key : keys) {
-            Integer value = map.get(key);
-
-            if (value > max) {
-                max = value;
-                result = key;
+            if (map.get(num) > max) {
+                max = map.get(num);
+                result = num;
             }
         }
+
+//        Set<Integer> keys = map.keySet();
+//        for (Integer key : keys) {
+//            Integer value = map.get(key);
+//
+//            if (value > max) {
+//                max = value;
+//                result = key;
+//            }
+//        }
 
         return result;
     }
