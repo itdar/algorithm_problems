138a668c9a6efd0ed8d918485690fc74cee7ebbb
itdar <thesockerrr@gmail.com>
2021-10-26 16:55:00 +0900

    chore: Solved LeetCode_1920_BuildArrayFromPermutation

---
+++ algorithm_sites/leetcode/LeetCode_1920_BuildArrayFromPermutation.java
@@ -0,0 +1,15 @@
+package algorithm_sites.leetcode;
+
+public class LeetCode_1920_BuildArrayFromPermutation {
+
+    public int[] buildArray(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; ++i) {
+            result[i] = nums[nums[i]];
+        }
+
+        return result;
+    }
+
+}
