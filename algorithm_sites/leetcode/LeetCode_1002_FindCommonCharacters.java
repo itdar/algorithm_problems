17284ae02edf1fd71e5ab35f6171d6e475766153
itdar <thesockerrr@gmail.com>
2021-11-11 23:58:26 +0900

    chore: Solved LeetCode_1002_FindCommonCharacters

---
+++ algorithm_sites/leetcode/LeetCode_1002_FindCommonCharacters.java
@@ -0,0 +1,23 @@
+package algorithm_sites.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LeetCode_1002_FindCommonCharacters {
+
+    public List<String> commonChars(String[] words) {
+        List<String> results = new ArrayList<>();
+
+        if (words.length == 1) {
+            results.add(words[0]);
+            return results;
+        }
+
+
+
+        return results;
+    }
+
+}
