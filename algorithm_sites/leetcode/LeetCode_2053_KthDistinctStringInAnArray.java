cd7b0602810d4b1b650882dfd7d13ede8aad30c8
itdar <thesockerrr@gmail.com>
2021-11-17 23:13:58 +0900

    chore: Solved LeetCode_2053_KthDistinctStringInAnArray

---
+++ algorithm_sites/leetcode/LeetCode_2053_KthDistinctStringInAnArray.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LeetCode_2053_KthDistinctStringInAnArray {
+
+    public String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+
+        for (int i = 0; i < arr.length; ++i) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+            if (stringIntegerEntry.getValue() == 1) {
+                --k;
+
+                if (k == 0) {
+                    return stringIntegerEntry.getKey();
+                }
+            }
+        }
+
+        return "";
+    }
+
+    public static void main(String args[]) {
+        LeetCode_2053_KthDistinctStringInAnArray test = new LeetCode_2053_KthDistinctStringInAnArray();
+
+        String[] input = new String[] {"d", "b", "c", "b", "c", "a"};
+        int k = 2;
+
+        String result = test.kthDistinct(input, k);
+
+        System.out.println("result: " + result);
+    }
+
+}
