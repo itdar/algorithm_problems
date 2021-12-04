14c3e9539919e1a453e52db1cc0d445affa9f51b
itdar <thesockerrr@gmail.com>
2021-12-04 17:43:06 +0900

    chore: LeetCode_2085_CountCommonWordsWithOneOccurrence with Testcase

--- src/main/java/algorithm_sites/leetcode/LeetCode_2085_CountCommonWordsWithOneOccurrence.java
+++ src/main/java/algorithm_sites/leetcode/LeetCode_2085_CountCommonWordsWithOneOccurrence.java
@@ -1,18 +1,57 @@
 package algorithm_sites.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LeetCode_2085_CountCommonWordsWithOneOccurrence {
 
+    // Process
+    // 1. Input 2 string arrays
+    // 2. 한개 배열씩 반복한다.
+    //  2.1. 중복안되는 것들의 배열을 각각 만든다.
+    // 3. 중복안되는 것들의 배열 2개에서 서로 중복되는 개수를 센다.
+    // 4. 개수 반환한다.
     public int countWords(String[] words1, String[] words2) {
         int result = 0;
 
+        Set<String> repeated = new HashSet<>();
+        Set<String> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
 
-        Map<String, Integer> map = new HashMap<>();
-
+        // 2.
         for (String word : words1) {
-            map.put(word, map.getOrDefault(map, 1)+1);
+            if (repeated.contains(word)) {
+                continue;
+            }
+
+            if (set1.contains(word)) {
+                set1.remove(word);
+                repeated.add(word);
+                continue;
+            }
+
+            set1.add(word);
+        }
+
+        for (String word : words2) {
+            if (repeated.contains(word)) {
+                continue;
+            }
+
+            if (set2.contains(word)) {
+                set2.remove(word);
+                repeated.add(word);
+                continue;
+            }
+
+            set2.add(word);
+        }
+
+        // 3.
+        for (String string : set1) {
+            if (set2.contains(string)) {
+                ++result;
+            }
         }
 
         return result;
--- src/test/java/algorithm_sites/leetcode/LeetCode_2085_CountCommonWordsWithOneOccurrenceTest.java
+++ src/test/java/algorithm_sites/leetcode/LeetCode_2085_CountCommonWordsWithOneOccurrenceTest.java
@@ -10,7 +10,9 @@ public class LeetCode_2085_CountCommonWordsWithOneOccurrenceTest {
     public void 두개의_배열에서_각각_1회씩만_나오면서_겹치는_숫자의_개수() {
         LeetCode_2085_CountCommonWordsWithOneOccurrence test = new LeetCode_2085_CountCommonWordsWithOneOccurrence();
 
+        assertThat(test.countWords(new String[] {"leetcode", "is", "amazing", "as", "is"}, new String[] {"amazing", "leetcode", "is"})).isEqualTo(2);
         assertThat(test.countWords(new String[] {"b","bb","bb"}, new String[] {"a","aa","aaa"})).isEqualTo(0);
+        assertThat(test.countWords(new String[] {"a", "ab"}, new String[] {"a","a", "a", "ab"})).isEqualTo(1);
     }
 
 }
