98db54b2b1793c2ee57ba0f92347bbdbbb27aa9e
itdar <thesockerr@gmail.com>
2022-06-17 02:36:26 +0900

    chore: 프로그래머스 답 추가

--- src/main/java/algorithm_sites/programmers/KkoInternship2019Winter_Tuple.java
+++ src/main/java/algorithm_sites/programmers/KkoInternship2019Winter_Tuple.java
@@ -3,10 +3,13 @@ package algorithm_sites.programmers;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class KkoInternship2019Winter_Tuple {
 
@@ -57,4 +60,33 @@ public class KkoInternship2019Winter_Tuple {
         return answer;
     }
 
+}
+
+class KkoInternship2019Winter_Tuple_Solution {
+    public int[] solution(String s) {
+        String[] stringArray = s.substring(2, s.length() - 2).split("},\\{");
+        Set<Integer> result = new LinkedHashSet<>();
+
+
+        // inputMap
+        // 3: 1,2,3
+        // 2: 2,1
+        // 4: 1,2,4,3
+        // 1: 2
+        Map<Integer, int[]> inputMap = new HashMap<>();
+        for (String a : stringArray) {
+            int[] temp = Stream.of(a.split(",")).mapToInt(Integer::parseInt).toArray();
+            inputMap.put(temp.length, temp);
+        }
+
+        // key 값 1 부터 사이즈 만큼 순회
+        for (int i=1; i<=inputMap.keySet().size(); i++) {
+            int[] temp = inputMap.get(i);
+            for (int t : temp) {
+                result.add(t);
+            }
+        }
+
+        return result.stream().mapToInt(i->i).toArray();
+    }
 }
