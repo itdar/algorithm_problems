38b7fdc77b64c1e80f9f367c4be2ee4b4ed25bdd
itdar <thesockerr@gmail.com>
2023-03-06 23:53:47 +0900

    chore: leetcode

---
+++ src/main/java/algorithm_sites/leetcode2/LC_00206_ReverseLinkedList.java
@@ -0,0 +1,17 @@
+package algorithm_sites.leetcode2;
+
+public class LC_00206_ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+}
