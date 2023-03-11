64689a57c28d3047ead53dd104d2ac0813d62218
itdar <thesockerr@gmail.com>
2023-03-11 23:57:53 +0900

    chore: leetcode

--- src/main/java/algorithm_sites/leetcode2/LC_00003_LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/algorithm_sites/leetcode2/LC_00003_LongestSubstringWithoutRepeatingCharacters.java
@@ -5,23 +5,6 @@ import java.util.List;
 
 public class LC_00003_LongestSubstringWithoutRepeatingCharacters {
 
-//    public int lengthOfLongestSubstring(String s) {
-//        List<Character> substringL = new ArrayList<>();
-//        int largestlength = 0;
-//        for(int right = 0; right < s.length(); right++) {
-//            if(substringL.contains(s.charAt(right))) {
-//                // get the index of the char
-//                int index = substringL.indexOf(s.charAt(right));
-//                substringL.remove(index);
-//                if(index > 0)
-//                    substringL.subList(0, index).clear();
-//            }
-//            substringL.add(s.charAt(right));
-//            largestlength = Math.max(largestlength, substringL.size());
-//        }
-//        return largestlength;
-//    }
-
     public int lengthOfLongestSubstring(String s) {
         int max = 0;
 
